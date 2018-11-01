ba66fd798937980ddc3a10de1cc5ffd20b856fe8
Guilherme Baufaker Rêgo <gbaufake@redhat.com>
2018-11-01 20:34:42 -0300

    KIALI-1857 Updating Traffic Generator

--- traffic-generator/docker/traffic-generator.sh
+++ traffic-generator/docker/traffic-generator.sh
@@ -6,5 +6,5 @@ for i in "${!array[@]}"
 do
     echo "GET ${array[i]}" >> targets.txt
 done
-vegeta attack -targets=targets.txt -rate=${RATE} -duration=${DURATION} > results.bin
-cat results.bin | vegeta report
+touch results.bin
+vegeta attack -targets=targets.txt -rate=${RATE} -duration=${DURATION} >> results.bin & tail -f results.bin | vegeta report
