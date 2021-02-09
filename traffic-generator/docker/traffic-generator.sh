1713611645234e29e51f01efd06920bce2a17766
Filip Brychta <fbrychta@redhat.com>
2021-02-09 15:02:05 +0100

    Add silent mode (#72)

--- traffic-generator/docker/traffic-generator.sh
+++ traffic-generator/docker/traffic-generator.sh
@@ -6,12 +6,19 @@ for i in "${!array[@]}"
 do
     echo "GET ${array[i]}" >> targets.txt
 done
-touch results.json
-vegeta attack -targets=targets.txt -rate=${RATE} -duration=${DURATION} -insecure >> results.json &
-sleep 3
-while true; do
-   vegeta report results.json
-   sleep 2
-   echo ""
-   echo ""
-done
+
+if [ "${SILENT}" = "true" ]
+then
+  vegeta attack -targets=targets.txt -rate=${RATE} -duration=${DURATION} -insecure > /dev/null
+else
+  touch results.json
+  vegeta attack -targets=targets.txt -rate=${RATE} -duration=${DURATION} -insecure >> results.json &
+  sleep 3
+  while true; do
+     vegeta report results.json
+     sleep 2
+     echo ""
+     echo ""
+  done
+
+fi
